816ac09edd9c505220b6277aa397f4b17300a966
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-04-08 18:59:21 +0300

    add: added game field

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-function App() {
+import { useEffect, useState } from "react";
+
+const width = 8;
+const colors = [
+  '#28a8e5',
+  '#bb1bbb',
+  '#f92121',
+  '#07d307',
+  '#ffff28',
+  '#ffae18'
+];
+
+const App = () => {
+  const [currentColors, setCurrentColors] = useState([]);
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColors(randomColorArrangement);
+  }
+
+  useEffect(() => {
+    createBoard();
+  }, [])
+
   return (
-    <div className="App">
-      Hello world
+    <div className="app">
+      <div className="app-game">
+        {currentColors.map((item, index) => (
+          <img key={index} style={{ backgroundColor: item }} alt={item} />
+        ))}
+      </div>
     </div>
   );
 }
